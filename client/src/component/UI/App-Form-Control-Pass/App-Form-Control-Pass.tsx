fc2fa5a65315774ddf3e348c633a941ce3571668
gaponmycap <gaponmycap@gmail.com>
2023-06-23 16:39:03 +0300

    Code refactoring.

--- client/src/component/UI/App-Form-Control-Pass/App-Form-Control-Pass.tsx
+++ client/src/component/UI/App-Form-Control-Pass/App-Form-Control-Pass.tsx
@@ -18,7 +18,7 @@ export function AppFormControlPass( { labelName, fieldName, errorMessage, regist
    const { isShow, handleClick } = useHidePass();
 
    return (
-       <FormControl isInvalid={ Boolean(errorMessage) }
+       <FormControl isInvalid={ Boolean( errorMessage ) }
                     isRequired={ isRequired ? isRequired : false }
                     h={ 100 }>
 
@@ -26,7 +26,7 @@ export function AppFormControlPass( { labelName, fieldName, errorMessage, regist
 
           <InputGroup>
 
-             <Input { ...register(fieldName) }
+             <Input { ...register( fieldName ) }
                     autoComplete={ "" }
                     focusBorderColor={ MAIN_COLOR_LIGHTER }
                     color={ FONT_COLOR }
@@ -51,7 +51,7 @@ export function AppFormControlPass( { labelName, fieldName, errorMessage, regist
 
           </InputGroup>
 
-          <FormErrorMessage color={ "red.500" }>
+          <FormErrorMessage color={ ALERT_COLOR }>
              { errorMessage && errorMessage }
           </FormErrorMessage>
 
